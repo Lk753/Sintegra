32aa3cda007b7881f0b8a2e8f7f082750f7032ae
BBel777 <isabellasaojoseguedes@gmail.com>
2026-05-28 16:04:30 -0300

    Delete codigo directory

--- codigo/SQL/CREATE TABLE manutencao.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE manutencao (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    Horario DATETIME DEFAULT CURRENT_TIMESTAMP,
-    dispositivo VARCHAR(50),
-    evento_manutencao VARCHAR(100) DEFAULT 'Reinicialização do sistema'
-);
